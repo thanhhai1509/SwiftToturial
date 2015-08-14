04166193fe5a00f0708f4cac6a027c93b7dea829
thanhhai1509 <thanhhaiqtvn@gmail.com>
2015-08-14 15:36:16 +0700

    commit
    First Commit

---
+++ SwiftToturial/SwiftToturial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftToturial
+//
+//  Created by Thanh Hai on 8/14/15.
+//  Copyright (c) 2015 Thanh Hai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
